6c1a1e990cf2fbd2e71b703655588cd2b8cb7eb7
alexandre <alexandre@PLX300000000415>
2020-04-16 17:12:45 +0200

    Añadidas clases de DTOMercado, DaoImplMercado, IDAOMercado, Conexion, borrada la clase Class1

---
+++ Datos/DatosMercado/IDAOMercado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.DatosMercado
+{
+    interface IDAOMercado
+    {
+        DTOMercado CrearMercadoDAO(DTOMercado mercadoDTO);
+
+        DTOMercado RecuperarMercadoPorIdMercadoDAO(int id);
+
+        DTOMercado ModificarNombreMercado(int idEvento, string nombreMercado);
+
+        DTOMercado ModificarCuotaMercado(int idEvento, float cuota);
+    }
+}
