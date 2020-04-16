76fcd8c8c19b62eb073009a3d66f0eec6412628f
Rubenncio <49519667+Rubenncio@users.noreply.github.com>
2020-04-16 17:10:28 +0200

    add class daoEvento

---
+++ Datos/IDAOEvento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public interface IDAOEvento
+{
+    DTOEvento CrearEventoDAO(DTOEvento eventoDTO);
+
+    DTOEvento RecuperarEventoDAO(int id);
+
+    DTOEvento RecuperarNombreEventoDAO(string nombreevento);
+
+    // bool ModificarSaldoMonederoDTO(int idMonederoAmodificar, float saldoNuevo);
+
+    // DTOMonedero RecuperarMonederoPorIdUsuarioDAO(int idUsuario);
+}
