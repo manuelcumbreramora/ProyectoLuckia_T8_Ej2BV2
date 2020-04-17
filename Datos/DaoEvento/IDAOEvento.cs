51a67dd7375a21d06518113833ec1c37c10b0607
Rubenncio <49519667+Rubenncio@users.noreply.github.com>
2020-04-17 09:28:33 +0200

    add commit by developruben, changes on datos

---
+++ Datos/DaoEvento/IDAOEvento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IDAOEvento
+{
+    DTOEvento CrearEventoDAO(DTOEvento eventoDTO);
+
+    DTOEvento RecuperarEventoDAO(int id);
+
+    //DTOEvento RecuperarNombreEventoDAO(string nombreevento);
+
+    List<DTOEvento> RecuperarListaEvento();
+
+
+}  
